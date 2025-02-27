d2e7d8f57d55b12e000e5db5d141797001d6cd75
C0lute <aagermanovich0@gmail.com>
2025-02-27 14:19:36 +0300

    add site

---
+++ js/burger.js
@@ -0,0 +1,56 @@
+
+ window.onload = function(){ 
+    var flag = 1;
+    const nav = document.getElementById("nav");
+    const ul = document.getElementById("ul");
+    const li = document.getElementsByClassName("header-center");
+    const href = document.getElementsByClassName("a");
+    function check(){
+        if( window.matchMedia("(max-width: 1024px)").matches ){
+            nav.className = "nav_burger";
+            flag = 1;
+        } else {
+            nav.className = "nav";
+            ul.className = "ul";
+            li.className = "header-center";
+            for(var i=0; i< href.length;i++){
+                href[i].style.fontSize = "16px";
+            }
+        }
+      }
+      
+      check();
+      
+      window.addEventListener( 'resize', check );
+
+
+
+
+
+
+    const b = document.getElementById('elem').addEventListener('click', function(event) {
+        if (flag == 1){
+            flag = 2;
+            nav.className = "nav_burger_delay";
+            ul.className = "ul_burger";
+            for(var i=0; i< li.length;i++){
+                li[i].style.width = "100%";
+                li[i].style.height = "max-content";
+            }
+            for(var i=0; i< href.length;i++){
+                href[i].style.fontSize = "24px";
+            }
+            
+                
+        }
+        else{
+            flag = 1;
+            nav.className = "nav_burger";
+            ul.className = "ul";
+            li.className = "header-center";
+            for(var i=0; i< href.length;i++){
+                href[i].style.fontSize = "16px";
+            }
+        }
+    });
+};
